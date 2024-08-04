7d5a2c0ccd4341ae10f461b36a3c16c11b3cc059
AdsonFS <adson.santos.dev@gmail.com>
2024-08-04 15:29:42 -0300

    ✅ GoogleTest Library
    add gtest library and first test

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # LangLang
+
+## How run tests
+```bash
+g++ -I../googletest/googletest/include -I../googletest/googletest \
+    -L../googletest/build/lib -lgtest -lgtest_main -pthread \
+    tests/test_main.cpp -o test_main.out
+./test_main.out
+```
+
---
+++ src/lexi/lexi_scanner.h
@@ -0,0 +1,32 @@
+#ifndef LEXI_SCANNER_H
+#define LEXI_SCANNER_H
+
+#include "token.h"
+#include <fstream>
+#include <string>
+
+class LexiScanner {
+public:
+  LexiScanner(std::string fileContent);
+  /*void scan();*/
+  Token nextToken();
+
+private:
+  int position;
+
+  std::string fileContent;
+  
+  bool isDigit(char c);
+  bool isLetter(char c);
+  bool isOperator(char c);
+  bool isSemicolon(char c);
+  bool isWhitespace(char c);
+  bool isCmpOperator(char c);
+  
+  bool isEOF();
+
+  char nextChar();
+  void backChar();
+};
+
+#endif // LEXI_SCANNER_H
